e3d7dd646d0e6897c71c476dacfea6a422a8112f
Michael Bailey <michael.bailey@crypsisgroup.com>
2020-03-03 20:04:32 -0500

    Add run server script - this is important at least to get it to sleep for the db start

---
+++ ops_data/runserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Making sure DB is listening one last time before we do anything"
+# Yeah, not a GREAT way to do it
+sleep 10
+cd /paws-data-pipeline/
+echo "Importing database data"
+python load_paws_data.py
+echo "Loading API app"
+python ./src/api_server/api_app.py
