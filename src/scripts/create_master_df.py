dd883d228a540c88c117d974fcac73d59dcdfbe5
mike <mtdamert@gmail.com>
2020-04-29 18:13:22 -0400

    Added master dataframe changes from our group meeting

--- src/scripts/create_master_df.py
+++ src/scripts/create_master_df.py
@@ -27,31 +27,31 @@ def create_user_master_df(conn, table_name, query):
 
 def main(conn):
     # Create a simple table with the following values:
-    # ------------> master_db_key, petpoint_id, volgistics_id, salesforce_id
+    # ------------> master_db (primary key), petpoint_id, volgistics_id, salesforce_id, email_address
     # petpoint[[outcome_person_id]]
-    # volgistics[[volgistics_id]]
+    # volgistics[[number]]
     # salesforcecontacts[[account_id]]
 
     # Create a new master_df table
     print('Start Creating master_df')
-    master_df = create_user_master_df(
-        conn, 'master_df',
-        '(master_id INT PRIMARY KEY NOT NULL, petpoint_id text, volgistics_id text, salesforce_id text, email_address text )'
-    )
+    # master_df = create_user_master_df(
+        # conn, 'master_df',
+        # '(master_id INT PRIMARY KEY NOT NULL, petpoint_id text, volgistics_id text, salesforce_id text, email_address text )'
+    # )
 
     # Merge Petpoint data to the master_df
-    # todo: add when petpoint is used
-    # petpoint = pd.read_sql('select * from petpoint', conn)
-    # master_df = petpoint[['outcome_person_id']].rename(
-    #    columns={'outcome_person_id': 'petpoint_id', 'out_email': 'email_address'}).merge(master_df, how='left')
+    petpoint = pd.read_sql('select * from petpoint', conn)
+    master_df = petpoint[['outcome_person_id', 'out_email']].rename(columns={'outcome_person_id': 'petpoint_id', 'out_email': 'email_address'})
 
     # Merge Salesforce data to the master_df
     salesforcecontacts = pd.read_sql('select * from salesforcecontacts', conn)
-    master_df.merge(salesforcecontacts[['account_id', 'email']].rename(
-        columns={'account_id': 'salesforce_id'}), how='left')
+    master_df = master_df.merge(salesforcecontacts[['account_id', 'email_address']].rename(
+        columns={'account_id': 'salesforce_id'}), how='outer')
     print('  -Successfully created salesforcecontact (Primary key) in master_df')
 
     # Merge Volgistics data into the master dataframe
     volgistics = pd.read_sql('select * from volgistics', conn)
-    master_df.merge(volgistics[['number', 'email']].rename(columns={'email': 'email_address'}), how='left')
+    master_df = master_df.merge(volgistics[['number', 'email']].rename(columns={'number' : 'volgistics_id', 'email': 'email_address'}), how='outer')
     print('  -Successfully created volgistics row in master_df')
+    
+    master_df.to_sql('master_df', conn, if_exists='append', index_label='master_id')
