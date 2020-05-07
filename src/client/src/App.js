d14fadb12448120b12e8aeb77d21ebbbfca7626b
urirot <urirot.mail@gmail.com>
2020-05-07 07:08:10 -0400

    add react

---
+++ src/client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="Paws Data Pipeline">
+         <p>The current time is {currentTime}.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
