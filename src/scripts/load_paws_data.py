9e1c4d6531e4bdf6cd9cb56d8c48849a4d7c79de
Stephen Poserina <sposerina@gmail.com>
2020-05-26 19:29:42 -0400

    debugging session

--- src/scripts/load_paws_data.py
+++ src/scripts/load_paws_data.py
@@ -19,11 +19,20 @@ def load_to_postgres(csv_path, table_name, drop_first_col=False):
     df.columns = df.columns.str.lower().str.strip()
     df.columns = df.columns.str.replace(' ', '_')
     df.columns = df.columns.map(lambda x: re.sub(r'\.+', '_', x))
+    #temp table
+    df.to_sql(table_name + '_temp', engine, index=False, if_exists='replace')
+    
     
     # get conneciton from engine and use in with clause to automatically handle transaction cleanup
-    with engine.connect() as connection:
+    '''with engine.connect() as connection:
         connection.execute(f'DROP TABLE IF EXISTS {table_name}')
         # load dataframe into database table
         current_app.logger.info('Creating table: ' + table_name)
         df.to_sql(table_name, engine, index=False,)
-        current_app.logger.info('Finished creating generic table for: ' + table_name)
+        current_app.logger.info('Finished creating generic table for: ' + table_name)'''
+
+def find_new_rows():
+    with engine.connect() as connection:
+        rows = connection.execute(f'select * from volgistics_temp left join volgistics v on v.number = t.number where v.number is null')
+        for row in rows:
+
