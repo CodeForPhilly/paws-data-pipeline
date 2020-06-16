63570744d4f74de5c08958cf44ea54e681a8c3fa
Ben Bucior <bbucior@u.northwestern.edu>
2020-06-16 19:28:37 -0400

    Match among new data before master_df
    Avoiding inconsistencies in the new vs. updated rows in master_df by attempting to match the new records to each other, first.

--- src/scripts/match_data.py
+++ src/scripts/match_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import copy
 
 from fuzzywuzzy import fuzz
 from config import REPORT_PATH, engine
@@ -67,9 +68,9 @@ def start(added_or_updated_rows):
     # TODO: Still need to replace a few functions that have placeholder names:
     # Define _get_most_recent_table_records_from_postgres with help from Steve to extract the new names (SELECT * FROM X WHERE _MOST_RECENT)
     # _get_master_df to get the full master dataframe from postgres, and load in the data to match against historical data
+    # def _get_primary_key(source_name): return source_name+'_id' (?)
     # FIXME: added_or_updated_rows json has an irregular tuple input format instead of key:value or similar
 
-    # TODO: implement updated_rows
     if len(added_or_updated_rows['updated_rows']) > 0:  # any updated rows
         raise NotImplementedError("match_data.start cannot yet handle row updates.")
 
@@ -78,28 +79,38 @@ def start(added_or_updated_rows):
     # TODO: refactor out into a settings import, likely in the same place where the table names and keys are defined
     match_criteria = ['email', 'name']
     table_priority = ['salesforcecontacts', 'volgistics', 'petpoint', 'ClinicHQ']
-    
-    # Append fields from the source tables (Salesforce, volgistics, petpoint, etc.) in priority order, then normalize
+
+    # Recreate the normalized match_criteria of-record based on the available source data based on the table_priority order
+    master_df = _get_master_df()
+    master_fields = master_df.columns
     master_df = coalesce_fields_by_id(
-        _get_master_df(),
-        [_get_most_recent_table_records_from_postgres(x) for x in table_priority],  # pseudo code
+        master_df,
+        [_get_most_recent_table_records_from_postgres(x) for x in table_priority],
         match_criteria
     )
     master_df = normalize_table_for_comparison(master_df, match_criteria)
 
-    updated_master_rows = pd.DataFrame()
-    new_master_rows = pd.DataFrame()
-    # TODO: need to attempt to match the new rows first to each other, then to the master table, or
-    # come up with another logic to reconcile updates to multiple columns simultaneously (or how to represent those cases)
-    # e.g. Formerly salesforce-only now also has volgistics and petpoint keys.
-    # Also, how to handle which columns are being passed in the table normalization here???
+    # Summarize all of the new_rows from the previous step into a single dataframe of changes
+    new_df = pd.DataFrame({col: [] for col in match_criteria})  # init an empty dataframe to collect the new data in one place
     for table_name in table_priority:
         if table_name not in input_data['new_rows'].keys():
             continue  # df is empty or not-updated, so there's nothing to do
-        new_table_data = normalize_table_for_comparison(pd.DataFrame(input_data['new_rows'][table_name]))
-        
-        matches, left_only, right_only = join_on_all_columns(master_df, new_table_data)
-        updated_master_rows = updated_master_rows.append(matches)
-        new_master_rows = new_master_rows.append(right_only)
-        master_df = matches.append(left_only).append(right_only)
+        table_cols = copy.deepcopy(match_criteria)
+        table_cols.append(_get_primary_key(table_name))
+        new_table_data = normalize_table_for_comparison(pd.DataFrame(input_data['new_rows'][table_name]))[table_cols]
+        new_df = new_df.merge(new_table_data, how='outer')
+
+    # Run the join, then report only the original fields of interest
+    master_df['_temp_orig_id'] = np.arange(master_df.shape[0] + 1)
+    master_df_matching_cols = copy.deepcopy(match_criteria)
+    master_df_matching_cols.append('_temp_orig_id')
+    new_df['_temp_new_id'] = np.arange(new_df.shape[0] + 1)
+    new_df_matching_cols = copy.deepcopy(match_criteria)
+    new_df_matching_cols.append('_temp_new_id')
+
+    matches, left_only, right_only = join_on_all_columns(master_df[master_df_matching_cols], new_df[new_df_matching_cols])
+    new_master_rows = new_df[new_df['_temp_new_id'] in right_only['_temp_new_id']][master_fields]
+    updated_master_rows = new_df['_temp_new_id' in matches['_temp_new_id']][master_fields]
+    # and could get the former versions of those rows via a similar merge step or filter for master_df
+
     return {'new_matches': new_master_rows.to_dict(), 'updated_matches': updated_master_rows.to_dict()}
