6542987bb809e9d01ab15dad8aec7c693541813e
Ben Bucior <bbucior@u.northwestern.edu>
2020-08-31 20:39:35 -0400

    Finish initial pass over rewriting User matches
    Now need to update this code to account for changes to the users
    and matching tables.

--- src/server/pipeline/match_data.py
+++ src/server/pipeline/match_data.py
@@ -97,6 +97,8 @@ def start(connection, added_or_updated_rows):
     # TODO: how will the names/emails be added to users--who is adding the new row to master?  For now, assume it's already been handled
     # TODO: adding an assertion as such as: assert sum(Users['master_id'].isna()) == 0
 
+    # WARNING: matching logic cannot get tested due to an error in FK constraints (salesforcecontacts ID)
+
     if len(added_or_updated_rows['updated_rows']) > 0:  # any updated rows
         raise NotImplementedError("match_data.start cannot yet handle row updates.")
         # TODO: implement.  Given the new Users workflow, this will likely be just checking if name and email are updated.
@@ -104,6 +106,7 @@ def start(connection, added_or_updated_rows):
 
     orig_master = pd.read_sql_table('master', connection)  #.drop(columns=['created_date', 'archived_date'])
     updated_master = orig_master.copy()
+    updated_master_rows = pd.Series()
     users_df = pd.read_sql_table('user_info', connection)
 
     for table_name in MATCH_PRIORITY:
@@ -129,54 +132,16 @@ def start(connection, added_or_updated_rows):
             .merge(normalize_table_for_comparison(users_df, MATCH_FIELDS), how='left')
             [[table_master_key, 'master_id']]
         )
-        # TODO: save the results of this loop, compile all the tables into master, then return the results to the next step in the flow script.
 
+        # Save results of the loop
+        updated_master = updated_master.merge(table_to_master, how='left')
+        updated_master_keys = updated_master_keys.append(table_to_master[table_master_key])
 
-    # OLD CODE BELOW: finish the code above then delete the rest
+    updated_master_keys = updated_master_keys.drop_duplicates()
+    updated_master_rows = updated_master_keys.merge(updated_master, how='left')
+    # new will be similar, after adding the new User row logic at the beginning of this section.
+    # asserting that the keys are not null.  If they are, we need a way to autoincrement (a new function for that??),
+    # then adding in the new users, refreshing the users table, then running the matching logic.
+    # Then, we'll have both the updated and new rows to pass to the next step.
+    return {'new_matches': [], 'updated_matches': updated_master_rows.to_dict(orient='records')}
 
-    # Combine all of the loaded new_rows into a single dataframe
-    new_df = pd.DataFrame({col: [] for col in MATCH_FIELDS})  # init empty dataframe
-    for table_name in MATCH_PRIORITY:
-        if table_name not in added_or_updated_rows['new_rows'].keys():
-            continue  # df is empty or not-updated, so there's nothing to do
-            # FIXME: handling empty df here?
-        table_csv_key = _get_table_primary_key(table_name)
-        table_master_key = _get_master_primary_key(table_name)
-        table_cols = copy.deepcopy(MATCH_FIELDS)
-        table_cols.append(table_csv_key)
-
-        new_table_data = (
-            pd.DataFrame(added_or_updated_rows['new_rows'][table_name])
-            .pipe(_reassign_combined_fields, {master_col: DATASOURCE_MAPPING[table_name][table_col] for master_col, table_col in MATCH_MAPPING.items()})
-            [table_cols]
-            .pipe(lambda df: normalize_table_for_comparison(df, MATCH_FIELDS))
-            .rename(columns={table_csv_key: table_master_key})
-        )
-        new_df = new_df.merge(new_table_data, how='outer')
-
-    # Recreate the normalized MATCH_FIELDS of-record based on the available
-    # source data in MATCH_PRIORITY order since not stored in postgres
-    master_df = pd.read_sql_table('master', connection).drop(columns=['created_date', 'archived_date'])
-    source_dfs = [
-        _reassign_combined_fields(
-            _get_most_recent_table_records_from_postgres(connection, table),
-            {master_col: DATASOURCE_MAPPING[table][table_col] for master_col, table_col in MATCH_MAPPING.items()}
-        ).rename(columns={_get_table_primary_key(table): _get_master_primary_key(table)})
-        for table in MATCH_PRIORITY
-    ]
-    master_df = coalesce_fields_by_id(master_df, source_dfs, MATCH_FIELDS)
-    master_df = normalize_table_for_comparison(master_df, MATCH_FIELDS)
-
-    # Run the join, then report only the original fields of interest
-    matches, left_only, right_only = join_on_all_columns(master_df[MATCH_FIELDS], new_df[MATCH_FIELDS])
-
-    # Use the matching columns (name+email) to handle matching accounting, then drop the ID fields
-    new_master_rows = right_only.merge(new_df, how='inner').drop(columns=MATCH_FIELDS)
-
-    # TODO LATER, after getting the new fields working, first.  Also should report the old version of the row.
-    # updated_master_rows = coalesce_fields_by_id(
-    #    master_df[master_fields],  # TODO: still need to figure out this field
-    #    new_df['_temp_new_id' in matches['_temp_new_id']][master_fields]
-    # )
-
-    return {'new_matches': new_master_rows.to_dict(orient='records'), 'updated_matches': []}
