c0e1d3f228339c3e676fed0c1f0944d1f1f7e810
urirot <urirot.mail@gmail.com>
2020-09-01 20:56:25 -0400

    file uploader

--- src/server/api/file_uploader.py
+++ src/server/api/file_uploader.py
@@ -20,26 +20,26 @@ def validate_and_arrange_upload(file, destination_path):
     determine_upload_type(file, file_extension, destination_path)
 
 def determine_upload_type(file, file_extension, destination_path):
+    df = None
+
     if file_extension == 'csv':
-        dfs = []
         df = pd.read_csv(file.stream, encoding='iso-8859-1')
-        dfs.append(df)
         file.close()
     else:
-        dfs = excel_to_dataframes(file)
+        df = excel_to_dataframes(file)
+
     for src_type in CSV_HEADERS:
-        for df in dfs:
-            if set(CSV_HEADERS[src_type]).issubset(df.columns):
-                with lock:
-                    filename = secure_filename(file.filename)
-                    now = time.gmtime()
-                    now_date = time.strftime("%Y-%m-%d--%H-%M-%S", now)
-                    current_app.logger.info("  -File: " + filename + " Matches files type: " + src_type)
-                    df.to_csv(os.path.join(destination_path, src_type + '-' + now_date + '.csv'))
-                    clean_current_folder(destination_path + '/current', src_type)
-                    df.to_csv(os.path.join(destination_path + '/current', src_type + '-' + now_date + '.csv'))
-                    current_app.logger.info("  -Uploaded successfully as : " + src_type + '-' + now_date + '.' + file_extension)
-                    flash(src_type + " {0} ".format(SUCCESS_MSG), 'info')
+        if set(CSV_HEADERS[src_type]).issubset(df.columns):
+            with lock:
+                filename = secure_filename(file.filename)
+                now = time.gmtime()
+                now_date = time.strftime("%Y-%m-%d--%H-%M-%S", now)
+                current_app.logger.info("  -File: " + filename + " Matches files type: " + src_type)
+                df.to_csv(os.path.join(destination_path, src_type + '-' + now_date + '.csv'))
+                clean_current_folder(destination_path + '/current', src_type)
+                df.to_csv(os.path.join(destination_path + '/current', src_type + '-' + now_date + '.csv'))
+                current_app.logger.info("  -Uploaded successfully as : " + src_type + '-' + now_date + '.' + file_extension)
+                flash(src_type + " {0} ".format(SUCCESS_MSG), 'info')
     flash('ERROR Unrecognized data extract: ' + file.filename, 'error')
 
 def excel_to_dataframes(xls):
@@ -48,10 +48,11 @@ def excel_to_dataframes(xls):
         wb.save(tmp.name)
         for sheetname in wb.sheetnames:
             for item in DATASOURCE_MAPPING:
-                if item['sheetname'] == sheetname:
-                    tmp.seek(0)
-                    df = pd.read_excel(tmp.read(), sheetname)
-                    return df
+                if 'sheetname' in DATASOURCE_MAPPING[item]:
+                    if DATASOURCE_MAPPING[item]['sheetname'] == sheetname:
+                        tmp.seek(0)
+                        df = pd.read_excel(tmp.read(), sheetname)
+                        return df
 
 def clean_current_folder(destination_path, src_type):
     if os.listdir(destination_path):
