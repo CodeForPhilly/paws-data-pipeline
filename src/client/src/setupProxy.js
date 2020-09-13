69b04cd1fc62939f902d781208bd41e8e956aedf
urirot <urirot.mail@gmail.com>
2020-09-13 18:27:44 +0300

    add local npm run script

--- src/client/src/setupProxy.js
+++ src/client/src/setupProxy.js
@@ -2,8 +2,9 @@ const proxy = require('http-proxy-middleware');
 
 
 module.exports = function(app) {
+    console.log(process.env.IS_LOCAL)
     app.use(proxy('/api/**', {
-            target: process.env.IS_LOCAL ? 'http://localhost:3333' : 'http://server:5000'
+            target: process.env.IS_LOCAL === true ? 'http://localhost:3333' : 'http://server:5000'
         }
     ));
 }
