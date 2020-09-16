0e8980ab3835c37a69c1f44db83a5b0d6afc2d50
Dan Kelley <dan@keriton.com>
2020-09-15 21:43:43 -0400

    if there is a space in search, split into first and last name

--- src/server/api/common_api.py
+++ src/server/api/common_api.py
@@ -1,17 +1,29 @@
 from config import engine
 from api.api import common_api
-from flask import jsonify
+from flask import jsonify, flash, current_app
 from sqlalchemy.sql import text
 
 
 @common_api.route('/api/contacts/<search_text>', methods=['GET'])
 def get_contacts(search_text):
     with engine.connect() as connection:
-        #todo: add logic to grab names from all sources - 1.salesforce 2.volgistics 3.petpoint once you find one, return all unique names and email
-        query = text("select concat(first_name,' ',last_name) as name, email, contact_id from salesforcecontacts \
-            WHERE lower(first_name) like :search_text \
-            OR lower(last_name) like :search_text")
-        query_result = connection.execute(query, search_text='%{}%'.format(search_text.lower()))
+        search_text = search_text.lower()
+
+        names = search_text.split(" ")
+        if len(names) == 2:
+            first_name = names[1]
+            last_name = names[0]
+            # note: query is an AND and the first name starts with the value 
+            query = text("select concat(first_name,' ',last_name) as name, email, contact_id from salesforcecontacts \
+                WHERE lower(first_name) like :first_name \
+                AND lower(last_name) like :last_name order by last_name")
+            query_result = connection.execute(query, first_name='{}%'.format(first_name), last_name='%{}%'.format(last_name))
+        else:
+            #todo: add logic to grab names from all sources - 1.salesforce 2.volgistics 3.petpoint once you find one, return all unique names and email
+            query = text("select concat(first_name,' ',last_name) as name, email, contact_id from salesforcecontacts \
+                WHERE lower(first_name) like :search_text \
+                OR lower(last_name) like :search_text order by last_name")
+            query_result = connection.execute(query, search_text='%{}%'.format(search_text))
 
         results = jsonify({'result': [dict(row) for row in query_result]})
 
