bc8b6bdcbd8684a855ddbbbb4f6704dadb61aeda
c-simpson <c_github@s512.org>
2020-11-28 16:44:33 -0500

    Initial changes to support testing

--- src/server/api/admin_api.py
+++ src/server/api/admin_api.py
@@ -8,21 +8,28 @@ from pipeline import flow_script
 from config import engine
 from flask import send_file, request, redirect, jsonify, current_app, abort
 from api.file_uploader import validate_and_arrange_upload
-from config import RAW_DATA_PATH, OUTPUT_PATH, CURRENT_SOURCE_FILES_PATH, ZIPPED_FILES, LOGS_PATH
+from config import (
+    RAW_DATA_PATH,
+    OUTPUT_PATH,
+    CURRENT_SOURCE_FILES_PATH,
+    ZIPPED_FILES,
+    LOGS_PATH,
+)
 
-ALLOWED_EXTENSIONS = {'csv', 'xlsx'}
+ALLOWED_EXTENSIONS = {"csv", "xlsx"}
+
 
 def __allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # file upload tutorial
-@admin_api.route('/api/file', methods=['POST'])
+@admin_api.route("/api/file", methods=["POST"])
 def uploadCSV():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return redirect(request.url)
 
-    for file in request.files.getlist('file'):
+    for file in request.files.getlist("file"):
         if __allowed_file(file.filename):
             try:
                 validate_and_arrange_upload(file, RAW_DATA_PATH)
@@ -31,34 +38,39 @@ def uploadCSV():
             finally:
                 file.close()
 
-    return redirect('/')
+    return redirect("/")
 
 
-@admin_api.route('/api/files/<destination>', methods=['GET'])
+@admin_api.route("/api/files/<destination>", methods=["GET"])
 def files(destination):
-    current_app.logger.info('Start returning zip of all data')
-    if request.args.get('download_current_btn'):
+    current_app.logger.info("Start returning zip of all data")
+    if request.args.get("download_current_btn"):
         source = RAW_DATA_PATH + destination
-    if request.args.get('download_archived_btn'):
+    if request.args.get("download_archived_btn"):
         source = RAW_DATA_PATH
-    if request.args.get('download_output_btn'):
+    if request.args.get("download_output_btn"):
         source = OUTPUT_PATH
 
-    zip_name = destination + '_data_out'
+    zip_name = destination + "_data_out"
 
     try:
-        current_app.logger.info(shutil.make_archive(ZIPPED_FILES + zip_name, 'zip', source))
-        return send_file(ZIPPED_FILES + zip_name + '.zip', as_attachment=True,
-                         attachment_filename=zip_name + '.zip')
+        current_app.logger.info(
+            shutil.make_archive(ZIPPED_FILES + zip_name, "zip", source)
+        )
+        return send_file(
+            ZIPPED_FILES + zip_name + ".zip",
+            as_attachment=True,
+            attachment_filename=zip_name + ".zip",
+        )
     except Exception as e:
         return str(e)
 
 
-@admin_api.route('/api/listCurrentFiles', methods=['GET'])
+@admin_api.route("/api/listCurrentFiles", methods=["GET"])
 def listCurrentFiles():
     result = None
 
-    current_app.logger.info('Start returning file list')
+    current_app.logger.info("Start returning file list")
     file_list_result = os.listdir(CURRENT_SOURCE_FILES_PATH)
 
     if len(file_list_result) > 0:
@@ -67,27 +79,24 @@ def listCurrentFiles():
     return jsonify(result)
 
 
-@admin_api.route('/api/execute', methods=['GET'])
+@admin_api.route("/api/execute", methods=["GET"])
 def execute():
-    current_app.logger.info('Execute flow')
+    current_app.logger.info("Execute flow")
     flow_script.start_flow()
 
     current_time = datetime.now().ctime()
     statistics = getStatistics()
 
-    last_execution_details = {
-        "executionTime": current_time,
-        "stats": statistics
-    }
+    last_execution_details = {"executionTime": current_time, "stats": statistics}
 
-    last_execution_file = open(LOGS_PATH + 'last_execution.json', 'w')
+    last_execution_file = open(LOGS_PATH + "last_execution.json", "w")
     last_execution_file.write(json.dumps(last_execution_details))
     last_execution_file.close()
 
     return jsonify(success=True)
 
 
-'''
+"""
 @admin_api.route('/api/status', methods=['GET'])
 def checkStatus():
     with engine.connect() as connection:
@@ -99,33 +108,35 @@ def checkStatus():
         for row in query_result:
             results = dict(row)
         return jsonify(results)
-'''
+"""
 
 
-@admin_api.route('/api/statistics', methods=['GET'])
+@admin_api.route("/api/statistics", methods=["GET"])
 def listStatistics():
     try:
-        last_execution_file = open(LOGS_PATH + 'last_execution.json', 'r')
+        last_execution_file = open(LOGS_PATH + "last_execution.json", "r")
         last_execution_details = json.loads(last_execution_file.read())
         last_execution_file.close()
 
     except Exception as e:
-        return abort(404)
+        return jsonify(None)
 
     return jsonify(last_execution_details)
 
 
 def getStatistics():
     with engine.connect() as connection:
-        query = text("SELECT \
-            SUM(CASE WHEN salesforcecontacts_id is not null and volgistics_id is null and shelterluvpeople_id is null THEN 1 ELSE 0 END) AS \"Only SalesForce Contacts\", \
-            SUM(CASE WHEN volgistics_id is not null and shelterluvpeople_id is null and salesforcecontacts_id is null THEN 1 ELSE 0 END) AS \"Only Volgistics Contacts\", \
-            SUM(CASE WHEN shelterluvpeople_id is not null and volgistics_id is null and salesforcecontacts_id is null THEN 1 ELSE 0 END) AS \"Only Shelterluv Contacts\", \
-            SUM(CASE WHEN salesforcecontacts_id is not null and shelterluvpeople_id is not null and volgistics_id is null THEN 1 ELSE 0 END) AS \"Only Salesforcec & Shelterluv\", \
-            SUM(CASE WHEN salesforcecontacts_id is not null and volgistics_id is not null and shelterluvpeople_id is null THEN 1 ELSE 0 END) AS \"Only Salesforce & Volgistics\", \
-            SUM(CASE WHEN volgistics_id is not null and shelterluvpeople_id is not null and salesforcecontacts_id is null THEN 1 ELSE 0 END) AS \"Only Shelterluv & Volgistics\", \
-            SUM(CASE WHEN salesforcecontacts_id is not null and volgistics_id is not null and shelterluvpeople_id is not null THEN 1 ELSE 0 END) AS \"Salesforcec & Shelterluv & Volgistics\" \
-            FROM master")
+        query = text(
+            'SELECT \
+            SUM(CASE WHEN salesforcecontacts_id is not null and volgistics_id is null and shelterluvpeople_id is null THEN 1 ELSE 0 END) AS "Only SalesForce Contacts", \
+            SUM(CASE WHEN volgistics_id is not null and shelterluvpeople_id is null and salesforcecontacts_id is null THEN 1 ELSE 0 END) AS "Only Volgistics Contacts", \
+            SUM(CASE WHEN shelterluvpeople_id is not null and volgistics_id is null and salesforcecontacts_id is null THEN 1 ELSE 0 END) AS "Only Shelterluv Contacts", \
+            SUM(CASE WHEN salesforcecontacts_id is not null and shelterluvpeople_id is not null and volgistics_id is null THEN 1 ELSE 0 END) AS "Only Salesforcec & Shelterluv", \
+            SUM(CASE WHEN salesforcecontacts_id is not null and volgistics_id is not null and shelterluvpeople_id is null THEN 1 ELSE 0 END) AS "Only Salesforce & Volgistics", \
+            SUM(CASE WHEN volgistics_id is not null and shelterluvpeople_id is not null and salesforcecontacts_id is null THEN 1 ELSE 0 END) AS "Only Shelterluv & Volgistics", \
+            SUM(CASE WHEN salesforcecontacts_id is not null and volgistics_id is not null and shelterluvpeople_id is not null THEN 1 ELSE 0 END) AS "Salesforcec & Shelterluv & Volgistics" \
+            FROM master'
+        )
         query_result = connection.execute(query)
 
         # Need to iterate over the results proxy
