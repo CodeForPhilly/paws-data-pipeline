46173ca51e2626bfac4d3a8df512c41188fd5897
c-simpson <c_github@s512.org>
2021-01-25 22:07:51 -0500

    Comments and cleanup for IS_LOCAL in test_api

--- src/server/app.py
+++ src/server/app.py
@@ -8,12 +8,12 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = "super-secret"  # TODO: SECURITY  Change this!
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 30  # Seconds for timeout. 60 for testing.
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 300  # Seconds for timeout. 60 for testing.
 jwt = JWTManager(app)
 
 
 # def create_app():
-app.secret_key = "1u9L#*&I3Ntc"
+app.secret_key = "1u9L#*&I3Ntc"  # TODO: SECURITY  Change this!
 app.config["MAX_CONTENT_LENGTH"] = 500 * 1024 * 1024  # 500 Megs
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 from api.admin_api import admin_api
