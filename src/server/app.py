cd8968b5a8317dc179055f9bc4f9f0a4298c0e87
Dan Kelley <dan@keriton.com>
2021-01-31 15:57:21 -0500

    had to modify to work with uWSGI and return app

--- src/server/app.py
+++ src/server/app.py
@@ -2,10 +2,8 @@ import os
 
 from flask import Flask
 
-app = Flask(__name__)
-
-
 def create_app():
+    app = Flask(__name__)
     app.secret_key = '1u9L#*&I3Ntc'
     app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # 500 Megs
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -13,10 +11,10 @@ def create_app():
     from api.common_api import common_api
     app.register_blueprint(admin_api)
     app.register_blueprint(common_api)
-
+    return app
 
 if __name__ == "__main__":
     FLASK_PORT = os.getenv('FLASK_PORT', None)
 
-    create_app()
+    app = create_app()
     app.run(host='0.0.0.0', debug=True, port=FLASK_PORT)
