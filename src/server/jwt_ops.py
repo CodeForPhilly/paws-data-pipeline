6ce4c120f92a0fedf0f86e4f479efb1f33498b64
c-simpson <c_github@s512.org>
2021-02-02 21:34:17 -0500

    Removed old test code from jwt_ops

--- src/server/jwt_ops.py
+++ src/server/jwt_ops.py
@@ -25,67 +25,22 @@ def admin_required(fn):
     return wrapper
 
 
-# Provide a method to create access tokens. The create_access_token()
-# function is used to actually generate the token, and you can return
-# it to the caller however you choose.
-@app.route("/login", methods=["POST"])
-def login():
-    if not request.is_json:
-        return jsonify({"msg": "Missing JSON in request"}), 400
-
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    if not username:
-        return jsonify({"msg": "Missing username parameter"}), 400
-    if not password:
-        return jsonify({"msg": "Missing password parameter"}), 400
-
-    if username == "admin" and password == "admin":
-        accesslevel = "admin"
-    elif username == "test" and password == "test":
-        accesslevel = "user"
-    else:
-        return jsonify({"msg": "Bad username or password"}), 401
-
-    @jwt.user_claims_loader
-    def add_claims_to_access_token(identity):
-        return {"role": accesslevel}
-
-    # Identity can be any data that is json serializable
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token), 200
-
-
 @jwt.user_claims_loader
 def add_claims_to_access_token(accesslevel):
+    """ Adds role k/v to token """
     return {"role": accesslevel}
 
 
 def create_token(username, accesslevel):
-
+    """ Create a JWT *access* token for the specified user and role. 
+        Role is magically added by the user_claims_loader decorator 
+    """
     # Identity can be any data that is json serializable
     new_token = create_access_token(identity=username)
     # add_claims_to_access_token(accesslevel)
     return jsonify(access_token=new_token)
 
 
-# Protect a view with jwt_required, which requires a valid access token
-# in the request to access.
-@app.route("/protected", methods=["GET"])
-@jwt_required
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
-
-@app.route("/admin", methods=["GET"])
-@admin_required
-def admin_func():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
-
 def get_jwt_user():
+    """ Read the JWT and return the associated username """
     return get_jwt_identity()
