66c4a729f664e6a69bcce09e22a18d610f5db477
c-simpson <c_github@s512.org>
2021-03-04 22:03:11 -0500

    Added Admin hdr,  rmvd Admin page  frm UserHeader

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,25 +1,116 @@
-import React from "react";
+import React from 'react'
+
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import Header from "./components/Header";
+import Header, {AdminHeader} from "./components/Header";
 
 import HomePage from './pages/Home';
 import Admin from './pages/Admin';
 import DataView from './pages/DataView360/DataView360';
 import About from './pages/About';
 
-/*basic routing of the app*/
-export default function App(props) {
-    return (
-        <Router>
-            <Header/>
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+
+const getUser = () => sleep(1000).then(() => ({username: 'PAWS_user', role:'admin'})) //.then(() =>  Error)
+
+const AuthContext = React.createContext()
+function AuthProvider({children}) {
+  const [state, setState] = React.useState({
+    status: 'pending',
+    error: null,
+    user: null,
+  })
+  React.useEffect(() => {
+    getUser().then(
+      user => setState({status: 'success', error: null, user}),
+      error => setState({status: 'error', error, user: null}),
+    )
+  }, [])
+
+  return (
+    <AuthContext.Provider value={state}>
+      {state.status === 'pending' ? (
+        'Loading...'
+      ) : state.status === 'error' ? (
+        <div>
+          Oh no
+          <div>
+            <pre>{state.error.message}</pre>
+          </div>
+        </div>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuthState() {
+  const state = React.useContext(AuthContext)
+  const isPending = state.status === 'pending'
+  const isError = state.status === 'error'
+  const isSuccess = state.status === 'success'
+  const isAuthenticated = state.user && isSuccess
+  return {
+    ...state,
+    isPending,
+    isError,
+    isSuccess,
+    isAuthenticated,
+  }
+}
+
+function Footer() {
+  return <p>This is an awesome app!</p>
+}
+
+// function Header() {
+//   const {user} = useAuthState()
+//   return <p>Hello {user.username}</p>
+// }
+
+function Content() {
+  const {user} = useAuthState()
+  return <p>I am so happy to have you here as a  {user.role}.</p>
+}
+
+function UnauthenticatedHeader() {
+  return <p>Please login</p>
+}
+
+function UnauthenticatedContent() {
+  return <p>You must login to read the message</p>
+}
+
+function UnauthenticatedApp() {
+  return (
+    <>
+      <UnauthenticatedHeader />
+      <UnauthenticatedContent />
+    </>
+  )
+}
+
+function AuthenticatedApp() {
+
+    const {user} = useAuthState()
+
+
+  return (
+    <>
+<Router>
+            {user.role == 'admin' ?  <AdminHeader/> : <Header /> }
+            <h3>{user.username} is a {user.role}</h3>
             <Switch>
                 <Route exact path="/">
                     <HomePage/>
                 </Route>
-                <Route path="/upload">
-                    <Admin/>
-                </Route>
+                {user.role == 'admin' && 
+                    <Route path="/upload">
+                        <Admin/>
+                    </Route>
+                }               
                 <Route path="/about">
                     <About/>
                 </Route>
@@ -28,6 +119,27 @@ export default function App(props) {
                 </Route>
             </Switch>
         </Router>
-    );
+    </>
+  )
 }
 
+function Home() {
+  const {user} = useAuthState()
+  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />
+}
+
+function App() {
+
+  // const {username, role} = useAuthState()
+
+  return (
+    <AuthProvider>
+      <div>
+         {/* <p>(AuthProvider) Hi , you big</p> */}
+        <Home />
+      </div>
+    </AuthProvider>
+  )
+}
+
+ export default App
--- src/client/src/components/Header.js
+++ src/client/src/components/Header.js
@@ -3,7 +3,7 @@ import { Link as RouterLink } from "react-router-dom";
 import {AppBar, Button, Toolbar, Typography } from "@material-ui/core";
 import styles from "./styles/header.module.css";
 
-export default function Header(props){
+export  function AdminHeader(props){
 
   return(
         <AppBar position="static" id="header" className={styles.header} elevation={1}> 
@@ -11,6 +11,21 @@ export default function Header(props){
             <Typography className={styles.header_logo} variant="h6">PAWS Data Pipeline</Typography>
             <div style={{"display":"flex", "justifyContent":"space-between", "margin":"16px 6px 16px 16px"}}>
               <Button className={styles.header_link} component={RouterLink} to="/upload">Admin</Button>
+              <Button className={styles.header_link} component={RouterLink} to="/dataView">360 DataView</Button>
+              <Button className={styles.header_link} component={RouterLink} to="/about">About</Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+  );
+}
+
+export default function Header(props){
+
+  return(
+        <AppBar position="static" id="header" className={styles.header} elevation={1}> 
+          <Toolbar style={{"minWidth":"100", "dipslay":"flex", "justifyContent":"space-between"}}>
+            <Typography className={styles.header_logo} variant="h6">PAWS Data Pipeline</Typography>
+            <div style={{"display":"flex", "justifyContent":"space-between", "margin":"16px 6px 16px 16px"}}>
               <Button className={styles.header_link} component={RouterLink} to="/dataView">360 DataView</Button>
               <Button className={styles.header_link} component={RouterLink} to="/about">About</Button>
             </div>
