de58dd18a31ce1bb3e2364d364c494dd539b0085
Dan Kelley <dan@keriton.com>
2021-03-09 20:36:05 -0500

    moved CMDs to startServer to allow logging and more control

--- src/server/app.py
+++ src/server/app.py
@@ -13,7 +13,6 @@ app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 300  # Seconds for timeout. 60 for test
 jwt = JWTManager(app)
 
 
-# def create_app():
 app.secret_key = APP_SECRET_KEY
 app.config["MAX_CONTENT_LENGTH"] = 500 * 1024 * 1024  # 500 Megs
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
--- src/server/wsgi.py
+++ src/server/wsgi.py
@@ -1,6 +1,4 @@
-from app import create_app
-
-app = create_app()
+from app import app
 
 if __name__ == "__main__":
     app.run()
