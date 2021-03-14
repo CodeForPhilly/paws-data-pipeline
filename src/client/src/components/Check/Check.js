f023c480e40772f801096c3bda8ae914a80ce590
c-simpson <c_github@s512.org>
2021-03-13 20:01:36 -0500

    Hitting tesst_auth twice

--- src/client/src/components/Check/Check.js
+++ src/client/src/components/Check/Check.js
@@ -8,15 +8,65 @@ const { DateTime } = require("luxon");
 // USeful for generating 'expired token' errors
 const old_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYxNDcxNjk5NCwianRpIjoiY2Y2Y2U5OTAtZGExNS00ZjZmLTljNGUtNzdjZWJjZmFlZjU0IiwibmJmIjoxNjE0NzE2OTk0LCJ0eXBlIjoiYWNjZXNzIiwic3ViIjoiYmFzZV91c2VyIiwiZXhwIjoxNjE0NzE3Mjk0LCJyb2xlIjoidXNlciJ9.WyFfP40lINcjeNvWEmNhoBr1wYyjniNQuIdMjuwyY0s"
 
+
+
+
+
+
+
+
+
+
 export default function Check() {
+
+    const { access_token, setToken } = useToken();
 
     const [processStatus, setProcessStatus] = React.useState('loading');
     const [error, setError] = React.useState('');
     const [data, setData] = React.useState('');
 
-    const { access_token, setToken } = useToken();
+    let history = useHistory()
+ 
+    function authCheck() {
+        console.log('authCheck startfetch @ ' + DateTime.local().toFormat('HH:mm:ss.SSS'))
+        fetch('http://localhost:5000/api/user/test_auth',
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + access_token
+            }
+        })
+    
+        .then((response) => {
+            console.log('authCheck handle response @ ' + DateTime.local().toFormat('HH:mm:ss.SSS'))
+            if (!response.ok) {
+                //throw (String(response.status + ':' + response.statusText))
+                throw (response)
+            }
+            return response.json()
+        } )
+        .then((data) => {
+            console.log('authCheck data  @ ' + DateTime.local().toFormat('HH:mm:ss.SSS'))
+            setProcessStatus('complete');
+            setData(data);
+        })
+        .catch((e) => {
+            let errCode = e.status
+            let errText = e.statusText
+    
+            setToken(null)  // Clear the token to force login again
+            let errStr = String(e)
+            setProcessStatus('error');
+            setError(errStr);
+            console.log(errCode + ':' + errText)
+            history.push('/')
+            return e
+        });
+    
+    }  // 
 
-    // let history = useHistory();
+   
 
     // get the decoded payload and header
     var decoded = jwt.decode(access_token, { complete: true });
@@ -35,41 +85,9 @@ export default function Check() {
            console.log("In Check w/o a token")
         }
 
-                // setProcessStatus('loading');
-                fetch('http://localhost:5000/api/user/test_auth',
-                {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Bearer ' + access_token
-                    }
-                })
-     
-                .then((response) => {
-     
-                    if (!response.ok) {
-                        //throw (String(response.status + ':' + response.statusText))
-                        throw (response)
-                    }
-                    return response.json()
-                }
-                )
-                .then((data) => {
-                    setProcessStatus('complete');
-                    setData(data);
-                })
-                .catch((e) => {
-                    let errCode = e.status
-                    let errText = e.statusText
-     
-                    setToken(null)  // Clear the token to force login again
-                    let errStr = String(e)
-                    setProcessStatus('error');
-                    setError(errStr);
-                    console.log(errCode + ':' + errText)
-                  //  history.push('/')
-                    return e
-                });
+    console.log('Running authCheck @ ' + DateTime.local().toFormat('HH:mm:ss.SSS'))
+     authCheck();
+    console.log('After authCheck @ ' + DateTime.local().toFormat('HH:mm:ss.SSS'))
 
            
     }, [ processStatus, access_token]);
