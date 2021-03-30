72547faabad5c91df612624aaaf6416f46739d8e
c-simpson <c_github@s512.org>
2021-03-30 14:08:35 -0400

    Set max JWT timeout value to 2 hr
    The actual timeout value can be reduced (but not increased) for testing

--- src/server/app.py
+++ src/server/app.py
@@ -9,7 +9,10 @@ from secrets import JWT_SECRET, APP_SECRET_KEY
 app = Flask(__name__)
 
 app.config["JWT_SECRET_KEY"] = JWT_SECRET  
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 45  #TODO: See to full value   # Seconds for timeout. 60 for testing.
+app.config["JWT_MAX_TIMEOUT"] = 7200 #Seconds
+
+ # We'll use max for default but can be reduced for testing
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = app.config["JWT_MAX_TIMEOUT"] 
 jwt = JWTManager(app)
 
 
