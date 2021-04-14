bee37e88def2bb30e26b44705e74d63057e87692
uri <urirot.mail@gmail.com>
2021-04-14 09:11:01 -0400

    Restore about us page

--- src/client/src/components/Header.js
+++ src/client/src/components/Header.js
@@ -14,6 +14,7 @@ export  function AdminHeader(props){  // This one if user has the ADMIN role
 
               <Button className={styles.header_link} component={RouterLink} to="/admin">Admin</Button>
               <Button className={styles.header_link} component={RouterLink} to="/360view/search">360 DataView</Button>
+                <Button className={styles.header_link} component={RouterLink} to="/about">About us</Button>
              { /* <Button className={styles.header_link} component={RouterLink} to="/check">Check</Button> */ }
             </div>
           </Toolbar>
