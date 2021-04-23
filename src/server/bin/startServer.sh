85580f228df7e63858b77345cb089b4a0bd1af5d
c-simpson <c_github@s512.org>
2021-04-23 16:42:11 -0400

    New script to export secrets to environment

--- src/server/bin/startServer.sh
+++ src/server/bin/startServer.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 
 # we may want to switch this to a script which logs output, etc?
 echo "------------STARTING `date` ------------------"
 set FLASK_APP=server/app.py
 export FLASK_APP
-
+source bin/export_secrets.sh
 # This abomination ensures that the PG server has finished its restart cycle
 echo "SLEEPING.. WAITING FOR DB"; sleep 5; echo "WAKING"; alembic upgrade head; echo "DB SETUP";
 #; python -m flask run --host=0.0.0.0 --no-reload
