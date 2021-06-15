afbcc62ef204e3dff316350fda00a93a1cbecd4d
Donna St. Louis <donna3@wharton.upenn.edu>
2021-06-15 19:17:58 -0400

    Common front-end functions

--- src/client/src/utils/utils.js
+++ src/client/src/utils/utils.js
@@ -1,8 +1,9 @@
+import moment from "moment";
 
 export function formatPhoneNumber(phoneNumberString) {
     let retVal;
 
-    if(phoneNumberString) {
+    if (phoneNumberString) {
         const match = phoneNumberString.match(/^(\d{3})(\d{3})(\d{4})$/)
 
         if (match) {
@@ -11,4 +12,14 @@ export function formatPhoneNumber(phoneNumberString) {
     }
 
     return retVal;
+}
+
+export function getAnimalAge(epochTime) {
+    let dateOfBirth = moment(epochTime * 1000);
+    return moment().diff(dateOfBirth, 'years');
+}
+
+export function showAnimalAge(epochTime) {
+    const age = getAnimalAge(epochTime)
+    return (age === 1) ? `${age} year` : `${age} years`
 }
