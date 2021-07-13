6fa79cae58ec25f5f230d139638e2371ce2759ae
uri <urirot.mail@gmail.com>
2021-07-13 08:46:03 -0400

    add cronjob for scheduling raw data through API ingest

---
+++ src/server/api/API_ingest/shelterluv_api_handler.py
@@ -0,0 +1,97 @@
+import os
+import requests
+import csv
+import time
+
+from constants import CURRENT_SOURCE_FILES_PATH, BASE_DROPBOX_PATH
+from api.API_ingest.dropbox_handler import upload_file_to_dropbox
+
+try:
+    from secrets import SHELTERLUV_SECRET_TOKEN
+except ImportError:
+    # Not running locally
+    print("Couldn't get SHELTERLUV_SECRET_TOKEN from file, trying environment **********")
+    from os import environ
+
+    try:
+        SHELTERLUV_SECRET_TOKEN = environ['SHELTERLUV_SECRET_TOKEN']
+    except KeyError:
+        # Not in environment
+        # You're SOL for now
+        print("Couldn't get secrets from file or environment")
+
+
+def write_csv(json_data):
+    now = time.localtime()
+    now_date = time.strftime("%Y-%m-%d--%H-%M-%S", now)
+
+    path = CURRENT_SOURCE_FILES_PATH + "shelterluvpeople-" + now_date + ".csv"  # store file name to use for dropbox
+
+    file_handle = open(path, "w")
+
+    csv_writer = csv.writer(file_handle)
+
+    count = 0
+    for item in json_data:
+        if count == 0:
+            # Writing headers of CSV file
+            header = item.keys()
+            csv_writer.writerow(header)
+            count += 1
+
+        # Writing data of CSV file
+        csv_writer.writerow(item.values())
+
+    file_handle.close()
+
+    return path
+
+#################################
+# This script is used to fetch data from shelterluv API.
+# Please be mindful of your usage.
+# example: /people will fetch the data of all people. and send approximately 300 requests.
+# https://help.shelterluv.com/hc/en-us/articles/115000580127-Shelterluv-API-Overview
+#################################
+
+######## Insights ###############
+# Max result items is 100 - even though it's not specifically specified in the above reference
+# /people has all the data. it seems that /person/:id isn't used
+#################################
+
+''' Iterate over all shelterlove people and store in json file in the raw data folder
+We fetch 100 items in each request, since that is the limit based on our research '''
+def store_shelterluv_people_all():
+    offset = 0
+    LIMIT = 100
+    has_more = True
+    shelterluv_people = []
+
+    print("Start getting shelterluv contacts from people table")
+
+    #while has_more:
+    r = requests.get("http://shelterluv.com/api/v1/people?limit={}&offset={}".format(LIMIT, offset),
+                     headers={"x-api-key": SHELTERLUV_SECRET_TOKEN})
+    response = r.json()
+    shelterluv_people += response["people"]
+    has_more = response["has_more"]
+    offset += 100
+
+    print("Finish getting shelterluv contacts from people table")
+
+    print("Start storing latest shelterluvpeople results to container")
+    if os.listdir(CURRENT_SOURCE_FILES_PATH):
+        for file_name in os.listdir(CURRENT_SOURCE_FILES_PATH):
+            file_path = os.path.join(CURRENT_SOURCE_FILES_PATH, file_name)
+            file_name_striped = file_path.split('-')[0].split('/')[-1]
+
+            if file_name_striped == "shelterluvpeople":
+                os.remove(file_path)
+
+    file_path = write_csv(shelterluv_people)
+    print("Finish storing latest shelterluvpeople results to container")
+
+
+    print("Start storing " + BASE_DROPBOX_PATH + '/shelterluv/' + "results to dropbox")
+    upload_file_to_dropbox(file_path, BASE_DROPBOX_PATH + '/shelterluv/' + file_path.split('/')[-1])
+    print("Finish storing " + BASE_DROPBOX_PATH + '/shelterluv/' + "results to dropbox")
+
