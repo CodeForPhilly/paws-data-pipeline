632bf5cc5ee6739458b28b75078c7c192719fe25
bdeck8317 <bdeck8317@comcast.net>
2021-07-23 14:47:15 -0400

    Adding doc strings to edges_dicts

--- SFDC-Playground/Python_Code/rfm_funcs/edges_dicts.py
+++ SFDC-Playground/Python_Code/rfm_funcs/edges_dicts.py
@@ -1,6 +1,11 @@
 def create_bins_dict(recency_edges, recency_labels, frequency_edges, frequency_labels, monetary_edges, monetary_labels):
-    '''
+    '''Create_bins_dict-- creates dictionaries for each edge and label pairing
+    This function takes in user defined bin edges and respective labels per each bin edge. User should
+    input a list of edges and labels in corresponding order. A set of edges and bins for each score should be entered.
 
+    e.g.
+    recency_edges = np.array([0, 1., 2.,4., 10.])
+    labels = list(1, 2, 3, 4, 5)
     '''
 
     recency_dict = {}
