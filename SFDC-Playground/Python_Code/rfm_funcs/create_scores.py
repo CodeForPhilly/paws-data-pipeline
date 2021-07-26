782baa8d391eb97180e5e486af243dcbf37c166f
bdeck8317 <bdeck8317@comcast.net>
2021-07-26 15:46:13 -0400

    creating one single function call file and a single create scores function which pulls data from tables and creates scores based on tabeled bins

--- SFDC-Playground/Python_Code/rfm_funcs/create_scores.py
+++ SFDC-Playground/Python_Code/rfm_funcs/create_scores.py
@@ -12,12 +12,26 @@ def create_scores():
 
     df = df.dropna(subset=['amount', 'close_date'])
 
+    # read in labels and bin edges from table
+
+    recency_labels = [5,4,3,2,1]
+    recency_bins =                  #        imported from table
+
+    frequency_labels= [1,2,3,4,5]
+    frequency_bins =                #       imported from table
+
+    monetary_labels= [ 1,2,3,4,5]
+    monetary_bins =                 #       imported from table
+
+
     # recency
+
 
     donations_2021 = df
     donations_2021['close_date'] =pd.to_datetime(donations_2021['close_date']).dt.date
-    from data_difference import date_difference
 
+        # calculate date difference between input date and
+    from rfm_functions import date_difference
     days = []
     for ii in donations_2021['close_date']:
         days.append(date_difference(ii, '2021-01-01'))
@@ -25,5 +39,48 @@ def create_scores():
 
     grouped_2021 = donations_2021.groupby('_id').agg({'days_since': ['min']}).reset_index()
 
-    labels = [5,4,3,2,1]
-    grouped_2021['recency_score'] = pd.qcut(grouped_2021[('days_since','min')], q= [0., .2, .4,.6, .8, 1], labels=labels)
+
+    grouped_2021['recency_score'] = pd.cut(grouped_2021[('days_since','min')], bins= recency_bins, labels=recency_labels, include_lowest = True)
+
+
+
+    # frequency
+
+    df['close_date'] = pd.DatetimeIndex(df['close_date'])
+
+    df_grouped = df.groupby(['matching_id', pd.Grouper(key = 'close_date', freq = 'Q')]).count().max(level=0)
+
+    df_grouped = df_grouped.reset_index()
+
+
+    df_frequency = df_grouped[['matching_id' , 'opp_id']]
+
+    df_frequency['frequency_score'] = pd.cut(df_frequency['opp_id'],
+                                               bins = frequency_bins, labels=frequency_labels, include_lowest=True)
+
+
+
+    # amount
+
+    df_amount = df.groupby(df['matching_id'], as_index=False).amount.max()
+
+    df_amount['amount_score'], bins = pd.cut(df_amount['amount'], bins= monetary_bins, include_lowest=True, labels = monetary_labels)
+
+
+    # Concatenate rfm scores
+        # merge monetary df and frequency df
+    df_semi = df_amount.merge(df_frequency, left_on='matching_id', right_on= 'matching_id')
+        # merge monetary/frequency dfs to recency df
+    df_final = df_semi.merge(grouped_2021, left_on='matching_id', right_on= 'matching_id')
+
+    # import function: rfm_concat, which will catenate integers as a string and then convert back to a single integer
+    from rfm_functions import rfm_concat
+    df_final = rfm_concat(df_final[('recency_score'),''], df_final['frequency_score'], df_final['amount_score'])
+
+    # Append rfm score to final df
+    df_final['rfm_score'] = rfm_score
+
+    from rfm_functions import merge_series
+    score_tuples = merge_series((df_final['matching_id']), df_final['rfm_score'])
+
+    return score_tuples
--- SFDC-Playground/Python_Code/rfm_funcs/rfm_functions.py
+++ SFDC-Playground/Python_Code/rfm_funcs/rfm_functions.py
@@ -1,3 +1,20 @@
+# rfm_funcs
+
+### Date difference for recency score calculation
+
+# get date difference
+def date_difference(my_date, query_date):
+    from datetime import datetime, date
+
+    d1 = datetime.strptime(str(my_date), "%Y-%m-%d")
+    d2 = datetime.strptime(str(query_date), "%Y-%m-%d")
+    diff = (d2 - d1)
+    return diff
+
+
+
+
+
 def rfm_concat(days_score, frequency_score, amount_score):
     '''
     This function takes in three pandas.series columns and returns a concatenated version of each score for a total rfm score.
@@ -15,4 +32,13 @@ def rfm_concat(days_score, frequency_score, amount_score):
     for ii, jj, kk in zip(days_score, frequency_score, amount_score):
         rfm_score.append(concat(ii,jj,kk))
 
+
+
     return rfm_score
+
+
+
+def merge_series(list1, list2):
+
+    merged_list = tuple(zip(list(list1), list(list2)))
+    return merged_list
