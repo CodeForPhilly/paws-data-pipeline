01e6966d71f5022fded42d854305c9b499be6042
Stephen Poserina <stephen.poserina@thinaer.io>
2022-12-29 20:06:45 -0500

    refactored code with better script names

--- src/server/api/API_ingest/ingest_sources_from_api.py
+++ src/server/api/API_ingest/ingest_sources_from_api.py
@@ -1,10 +1,10 @@
-from api.API_ingest import shelterluv_api_handler, salesforce_api_handler
+from api.API_ingest import shelterluv_people, salesforce_contacts
 import structlog
 logger = structlog.get_logger()
 
 def start(session):
     logger.debug("Start Fetching raw data from different API sources")
     #Run each source to store the output in dropbox and in the container as a CSV
-    shelterluv_api_handler.store_shelterluv_people_all(session)
-    salesforce_api_handler.store_contacts_all(session)
+    shelterluv_people.store_shelterluv_people_all(session)
+    salesforce_contacts.store_contacts_all(session)
     logger.debug("Finish Fetching raw data from different API sources")
