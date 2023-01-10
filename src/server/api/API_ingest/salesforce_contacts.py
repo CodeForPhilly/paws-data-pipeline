9ce4966d70ffa3b6977013d9fe84cd58056e5063
c-simpson <c_github@s512.org>
2023-01-09 21:09:23 -0500

    Tweaks to store_contacts_all()

--- src/server/api/API_ingest/salesforce_contacts.py
+++ src/server/api/API_ingest/salesforce_contacts.py
@@ -20,20 +20,26 @@ def store_contacts_all():
 
         logger.debug("retrieving the latest salesforce contacts data")
 
-        if not os.path.exists('bin/connected-app-secrets.pem'):
-            logger.warn("missing salesforce jwt private key, skipping data pull")
+        if os.path.exists('server/bin/connected-app-secrets.pem'):
+            pem_file = 'server/bin/connected-app-secrets.pem'
+        elif os.path.exists('bin/connected-app-secrets.pem'):
+            pem_file = 'bin/connected-app-secrets.pem'
+        else:
+            logger.error("Missing salesforce jwt private key pem file, skipping data pull")
             return
 
         sf = Salesforce(username=os.getenv('SALESFORCE_USERNAME'), consumer_key=os.getenv('SALESFORCE_CONSUMER_KEY'),
-                        privatekey_file='bin/connected-app-secrets.pem')
+                        privatekey_file='server/bin/connected-app-secrets.pem')
         results = sf.query("SELECT Contact_ID_18__c, FirstName, LastName, Contact.Account.Name, MailingCountry, MailingStreet, MailingCity, MailingState, MailingPostalCode, Phone, MobilePhone, Email FROM Contact")
         logger.debug("%d total Salesforce contact records", results['totalSize'])
         if TEST_MODE:
             logger.debug("running in test mode so only downloading first page of Salesforce contacts")
 
+        total_records = 0
         done = False
         while not done:
-            logger.debug("Query returned %d Salesforce contact records", len(results['records']))
+            total_records += len(results['records'])
+            logger.debug("Query returned %d Salesforce contact records, total %d", len(results['records']), total_records)
             for row in results['records']:
                 account_name = row['Account']['Name'] if row['Account'] is not None else None
                 contact = SalesForceContacts(contact_id=row['Contact_ID_18__c'],
