699e716b5102dc904d96e2a52ffa2c2690adf848
Stephen Poserina <stephen.poserina@thinaer.io>
2023-03-12 16:04:49 -0400

    added stub files and script for publishing a pipeline update message to Salesforce

--- src/server/api/internal_api.py
+++ src/server/api/internal_api.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
 import structlog
-from flask import jsonify
+from flask import jsonify, request
 
 from api.API_ingest import ingest_sources_from_api, salesforce_contacts
 from api.api import internal_api
 from rfm_funcs.create_scores import create_scores
 from api.API_ingest import updated_data
+from pub_sub import salesforce_message_publisher
+import json
 
 logger = structlog.get_logger()
 
@@ -51,3 +53,14 @@ def get_contact_data():
     contact_json =  updated_data.get_updated_contact_data()
     logger.debug("Returning %d contact records",  len(contact_json) )
     return jsonify(contact_json), 200
+
+
+@internal_api.route("/api/internal/salesforce_platform_message", methods=["POST"])
+def hit_salesforce_platform_message():
+    try:
+        post_dict = json.loads(request.data)
+        salesforce_message_publisher.pipeline_update_message(post_dict)
+    except Exception as e:
+        logger.error(e)
+
+    return jsonify({'outcome': 'OK'}), 200
