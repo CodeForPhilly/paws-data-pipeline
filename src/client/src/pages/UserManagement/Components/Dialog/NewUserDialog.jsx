cac994f93e86362b1973bddc9b365f2fd326f07f
Stephen Carroll <stephenalexandercarroll@gmail.com>
2023-04-20 14:56:00 -0400

    496: Add new user dialog form and validations

--- src/client/src/pages/UserManagement/Components/Dialog/NewUserDialog.jsx
+++ src/client/src/pages/UserManagement/Components/Dialog/NewUserDialog.jsx
@@ -1,8 +1,30 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@material-ui/core';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Typography } from '@material-ui/core';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { buildNameValidation, buildPasswordValidation, buildRoleValidation, buildUsernameValidation } from '../../Validations';
+
 
 export default function NewUserDialog(props) {
     const { onClose } = props;
+
+    const validationSchema = Yup.object().shape({
+        name: buildNameValidation(),
+        username: buildUsernameValidation(),
+        role: buildRoleValidation(),
+        password: buildPasswordValidation(),
+        confirmPassword: Yup.string().oneOf([Yup.ref("password")], "Passwords must match"),
+    });
+
+    const { register, handleSubmit, formState: { errors }, reset, getValues } = useForm({
+        resolver: yupResolver(validationSchema),
+    });
+
+    const onSubmitHandler = (data) => {
+        console.log({ data });
+        reset();
+    }
 
     return (
         <Dialog
@@ -10,57 +32,79 @@ export default function NewUserDialog(props) {
             fullWidth
             open
         >
-            <DialogTitle style={{ fontSize: '20px' }}>Create New User</DialogTitle>
-            <DialogContent>
-                <TextField
-                    margin="dense"
-                    id="full-name-input"
-                    label="Full Name"
-                    variant="standard"
-                    autoFocus
-                    fullWidth
-                />
-                <TextField
-                    margin="dense"
-                    id="username-input"
-                    label="Username"
-                    variant="standard"
-                    fullWidth
-                />
-                <TextField
-                    margin="dense"
-                    id="role-input"
-                    label="Role - user/editor/admin"
-                    variant="standard"
-                    fullWidth
-                />
-                <TextField
-                    margin="dense"
-                    id="password-input"
-                    label="Password"
-                    type="password"
-                    fullWidth
-                />
-                <TextField
-                    margin="dense"
-                    id="confirm-password-input"
-                    label="Confirm Password"
-                    type="password"
-                    fullWidth
-                />
-                <DialogActions>
-                    <Button
-                        onClick={onClose}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                    // type="submit"
-                    >
-                        Submit
-                    </Button>
-                </DialogActions>
-            </DialogContent>
+            <DialogTitle style={{ fontSize: "20px" }}>Create New User</DialogTitle>
+            <form onSubmit={handleSubmit(onSubmitHandler)}>
+                <DialogContent>
+                    <TextField
+                        {...register("name")}
+                        margin="dense"
+                        id="name-input"
+                        label="Name"
+                        variant="standard"
+                        autoFocus
+                        fullWidth
+                    />
+                    {errors.name &&
+                        <Typography color="error">{errors.name.message}</Typography>
+                    }
+                    <TextField
+                        {...register("username")}
+                        margin="dense"
+                        id="username-input"
+                        label="Username"
+                        variant="standard"
+                        fullWidth
+                    />
+                    {errors.username &&
+                        <Typography color="error">{errors.username.message}</Typography>
+                    }
+                    <TextField
+                        {...register("role")}
+                        margin="dense"
+                        id="role-input"
+                        label="Role - user/editor/admin"
+                        variant="standard"
+                        fullWidth
+                    />
+                    {errors.role &&
+                        <Typography color="error">{errors.role.message}</Typography>
+                    }
+                    <TextField
+                        {...register("password")}
+                        margin="dense"
+                        id="password-input"
+                        label="Password"
+                        type="password"
+                        fullWidth
+                    />
+                    {errors.password &&
+                        <Typography color="error">{errors.password.message}</Typography>
+                    }
+                    <TextField
+                        {...register("confirmPassword")}
+                        margin="dense"
+                        id="confirm-password-input"
+                        label="Confirm Password"
+                        type="password"
+                        fullWidth
+                    />
+                    {errors.confirmPassword &&
+                        <Typography color="error">{errors.confirmPassword.message}</Typography>
+                    }
+                    <DialogActions>
+                        <Button
+                            onClick={onClose}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
+                    </DialogActions>
+                </DialogContent>
+            </form>
         </Dialog>
     )
 
---
+++ src/client/src/pages/UserManagement/Validations.js
@@ -0,0 +1,43 @@
+import * as Yup from 'yup';
+
+export const DISALLOWED_WORDS = [
+    'cat',
+    'dog',
+    'password',
+
+]
+
+export const buildNameValidation = () => {
+    return Yup.string()
+        .min(2, "Name must be at least 2 characters")
+        .matches(/^[A-Za-z]+$/, "Name must only contain letters")
+        .required("Name is required")
+}
+
+export const buildUsernameValidation = () => {
+    return Yup.string()
+        .min(2, "Username must be at least 2 characters")
+        .matches(/^[a-zA-Z0-9].*$/, "Username must begin with a letter")
+        .matches(/^.*[a-zA-Z0-9]$/, "Username must end with a letter")
+        .matches(/^(?!.*?__)[a-zA-Z0-9_]+$/, "Username must contain only alphanumeric characters and non-consecutive underscores")
+}
+
+export const buildRoleValidation = () => {
+    return Yup.string()
+        .oneOf(["user", "editor", "admin"], "Role must be one of the following: user/editor/admin")
+        .required("Role is required")
+}
+
+export const buildPasswordValidation = () => {
+    return Yup.string()
+        .test(
+            "no-disallowed-words",
+            "Password cannot include 'dog', 'cat', 'password', or your username",
+            (value, context) => {
+                const lowercasePassword = value.toLowerCase();
+                const lowercaseUsername = context.parent.username.toLowerCase()
+                return [...DISALLOWED_WORDS, lowercaseUsername].every((word) => !lowercasePassword.includes(word))
+            })
+        .min(12, "Password must contain at least 12 letters")
+        .max(36, "Password must be 36 characters or less")
+}
