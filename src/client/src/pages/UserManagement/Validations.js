06c99ddd6b38b8d55d7ae809f58d38ed887827cb
Stephen Carroll <stephenalexandercarroll@gmail.com>
2023-04-21 14:19:48 -0400

    496: Trim whitespace in validations and disallow consecutive spaces in name

--- src/client/src/pages/UserManagement/Validations.js
+++ src/client/src/pages/UserManagement/Validations.js
@@ -9,27 +9,33 @@ export const DISALLOWED_WORDS = [
 
 export const buildNameValidation = () => {
     return Yup.string()
+        .trim()
         .min(2, "Name must be at least 2 characters")
-        .matches(/^[A-Za-z]+$/, "Name must only contain letters")
+        .matches(/^(?!.*  )[a-zA-Z ]+$/, "Name must only contain letters and non-consecutive internal spaces")
         .required("Name is required")
 }
 
 export const buildUsernameValidation = () => {
     return Yup.string()
+        .trim()
         .min(2, "Username must be at least 2 characters")
-        .matches(/^[a-zA-Z0-9].*$/, "Username must begin with a letter")
-        .matches(/^.*[a-zA-Z0-9]$/, "Username must end with a letter")
+        .matches(/^[a-zA-Z0-9].*$/, "Username must begin with a letter or number")
+        .matches(/^.*[a-zA-Z0-9]$/, "Username must end with a letter or number")
         .matches(/^(?!.*?__)[a-zA-Z0-9_]+$/, "Username must contain only alphanumeric characters and non-consecutive underscores")
 }
 
 export const buildRoleValidation = () => {
     return Yup.string()
+        .trim()
         .oneOf(["user", "editor", "admin"], "Role must be one of the following: user/editor/admin")
         .required("Role is required")
 }
 
 export const buildPasswordValidation = (username) => {
     return Yup.string()
+        .trim()
+
+
         .test(
             "no-disallowed-words",
             "Password cannot include 'dog', 'cat', 'password', or your username",
