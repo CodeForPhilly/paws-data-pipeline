8a9c4b263c5e8a2673722b2e169bd333e3935a39
Steve Carroll <stephenalexandercarroll@gmail.com>
2023-06-26 16:23:08 -0400

    492: styling

--- src/client/src/pages/Admin.js
+++ src/client/src/pages/Admin.js
@@ -156,37 +156,32 @@ export default function Admin(props) {
                                 </Paper>
                             </Grid>
                             {!_.isEmpty(statistics) && 
-                                <>
-                                    <Grid item>
-                                        <Typography variant="h5">Last Match Analysis</Typography>
-                                    </Grid>
-                                    <Grid item>
-                                        <TableContainer component={Paper}>
-                                            <Table aria-label="simple table">
-                                                <TableBody>
-                                                    <TableRow key='time'>
+                                <Grid item>
+                                    <TableContainer component={Paper} >
+                                        <Table aria-label="simple table">
+                                            <TableBody>
+                                                <TableRow key='time'>
+                                                    <TableCell align="left" component="th" scope="row">
+                                                        <b>Last Analysis</b>
+                                                    </TableCell>
+                                                    <TableCell align="left">
+                                                        <b>
+                                                            {moment(lastExecution, "dddd MMMM Do h:mm:ss YYYY").local().format("MMMM Do YYYY, h:mm:ss a")}
+                                                        </b>
+                                                    </TableCell>
+                                                </TableRow>
+                                                {statistics.map((row, index) => (
+                                                    <TableRow key={index}>
                                                         <TableCell align="left" component="th" scope="row">
-                                                            <b>Last Analysis</b>
+                                                            {row[0]}
                                                         </TableCell>
-                                                        <TableCell align="left">
-                                                            <b>
-                                                                {moment(lastExecution, "dddd MMMM Do h:mm:ss YYYY").local().format("MMMM Do YYYY, h:mm:ss a")}
-                                                            </b>
-                                                        </TableCell>
+                                                        <TableCell align="left">{row[1]}</TableCell>
                                                     </TableRow>
-                                                    {statistics.map((row, index) => (
-                                                        <TableRow key={index}>
-                                                            <TableCell align="left" component="th" scope="row">
-                                                                {row[0]}
-                                                            </TableCell>
-                                                            <TableCell align="left">{row[1]}</TableCell>
-                                                        </TableRow>
-                                                    ))}
-                                                </TableBody>
-                                            </Table>
-                                        </TableContainer>
-                                    </Grid>
-                                </>
+                                                ))}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </Grid>
                             }
                         </Grid>
                     </Grid>
